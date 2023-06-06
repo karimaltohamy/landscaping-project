1a6dfd4b70ca2f8140bf5fdd582e475cac7b1de8
Karim Altohamy <karimbadr2003@gmail.com>
2023-06-06 13:42:07 +0200

    comit

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -9,3 +9,21 @@ btnMenu.addEventListener("click", () => {
 btnClose.addEventListener("click", () => {
   menuMobile.classList.remove("active");
 });
+
+// read more
+const descRead = document.querySelector(".desc_read");
+const hiddenContent = document.querySelector(".text_more");
+const readMoreLinks = document.querySelectorAll(".btn_read_text");
+
+readMoreLinks.forEach((readMoreLink) => {
+  readMoreLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    readMoreLink.parentElement.children[0].classList.toggle("hide");
+
+    if (readMoreLink.parentElement.children[0].classList.contains("hide")) {
+      readMoreLink.textContent = "Read More";
+    } else {
+      readMoreLink.textContent = "Read Less";
+    }
+  });
+});
